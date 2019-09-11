44f37e8c932966e9eb39b127a524030164d943c0
qiushaowei <qiushaowei@360.net>
2019-09-11 17:48:06 +0800

    up

---
+++ backup.sh
@@ -0,0 +1,4 @@
+
+cp -rf ./top-history/manifest.json ./src/manifest.json
+cp -rf ./top-history/chrome ./src/ 
+cp -rf ./top-history/_locales ./src/ 
--- finishbuild.sh
+++ finishbuild.sh
@@ -1,5 +1,6 @@
 rm -rf ./top-history/icons*/manifest.* 
 cp -rf ./src/chrome ./top-history 
+cp -rf ./src/_locales ./top-history 
 cp -rf ./src/manifest.json ./top-history/manifest.json
 
 cp -rf ./top-history/index.html ./top-history/sync.html
