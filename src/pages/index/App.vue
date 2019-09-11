ab810032a46d01d9efc774172eb562b882b08fc2
qiushaowei <qiushaowei@360.net>
2019-09-11 14:38:53 +0800

    up

---
+++ src/pages/index/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+        <el-container style="height: 100%; border: 1px solid #eee">
+          <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+            <el-menu
+              default-active="1"
+              class="el-menu-vertical-demo"
+              >
+                <el-link href="index.html" target="_save_anywhere_index">
+                  <el-menu-item index="1">
+                    <i class="el-icon-s-home"></i>{{$t('mainPage')}}
+                  </el-menu-item>
+                </el-link>
+                <router-link to="importExport.html">
+                  <el-menu-item index="2">
+                    <i class="el-icon-upload2"></i>{{$t('importExport')}}
+                  </el-menu-item>
+                </router-link>
+
+                <router-link to="sync.html">
+                  <el-menu-item index="6" >
+                    <i class="el-icon-upload"></i>{{$t('sync')}}
+                  </el-menu-item>
+                </router-link>
+
+                <router-link to="dataManage.html" v-if="devMode">
+                  <el-menu-item index="3" >
+                    <i class="el-icon-data-board"></i>{{$t('dataManage')}}
+
+                  </el-menu-item>
+                </router-link>
+
+                <el-link href="popup.html" target="_save_anywhere_popup" v-if="devMode">
+                  <el-menu-item index="4">
+                    <i class="el-icon-folder-opened"></i>{{$t('popupPage')}} 
+                  </el-menu-item>
+                </el-link>
+
+                <router-link to="setting.html">
+                  <el-menu-item index="5">
+                    <i class="el-icon-setting"></i>{{$t('setting')}}
+                  </el-menu-item>
+                </router-link>
+            </el-menu>
+          </el-aside>
+            <router-view @[this.globalVar.updateDevModeEventName]="devModeChange"></router-view>
+        </el-container>
+  </div>
+</template>
+
+<style>
+@import './index.less';
+html {
+  width: 100%;
+  height: 100%;
+}
+body {
+  width: 100%;
+  height: 100%;
+  background: transparent;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+.el-link {
+    display: block;
+}
+</style>
+
+<script>
+window.name = '_save_anywhere_index';
+
+export default {
+	data() {
+		return {
+            devMode: this.globalVar.devMode
+		}
+	}
+    , components: {
+    }
+    , computed: {
+        
+    }
+	, methods: {
+		devModeChange( val ){
+			this.devMode = val;
+		}
+　　　　, refresh:function(){
+　　　　　　this.$router.go(0);
+　　　　}
+	}
+}
+</script>
+
+
---
+++ src/pages/index/mainLayout.vue
@@ -0,0 +1,152 @@
+<template>
+  <el-container>
+    <el-header style="text-align: left; font-size: 12px">
+        <el-row>
+            <el-col :span="10">
+                <span>
+                    <i class="el-icon-logo" style="margin-right: 15px"></i>
+                </span>
+                <label style="font-weight: bold; font-size: 16px;">{{packInfo.name}}</label>
+                <span>{{$t('totalLabel')}} </span><span>{{fullTotal}}</span>
+                <span v-if="fullTotal != listTotal">, {{$t('curTotalLabel')}} </span><span v-if="fullTotal != listTotal">{{listTotal}}</span>
+            </el-col>
+            <el-col :span="14" style="text-align:right;">
+                  <el-input
+                      :placeholder="$t('searchPlaceholder')"
+                      v-model="searchText"
+                      @input="onTextInput"
+                      >
+                      <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                  </el-input>    
+            </el-col>
+        </el-row>
+    </el-header>
+    
+    <el-main 
+		v-loading="loading"
+    >
+      <el-table :data="listData">
+        <el-table-column prop="id" label="ID" width="80" ></el-table-column>
+        <el-table-column  :label="$t('note')">
+            <template slot-scope="scope">
+            <div>
+                <a :href="scope.row.siteUrl" target="_blank">
+                    <label v-if="scope.row.siteTitle" style="display:block;font-weight:bold; margin-bottom:5px;"
+                    v-html="hightlightSearch(scope.row.siteTitle)"
+                    ></label>
+                </a>
+                    <span v-html="hightlightSearch(scope.row.note, 1, scope.row )"></span>
+                <a :href="scope.row.siteUrl" target="_blank">
+                    <label v-if="scope.row.siteUrl" style="display:block; margin-top:5px;">{{scope.row.siteUrl}}</label>
+                </a>
+            </div>
+            </template>
+        </el-table-column>
+        <el-table-column  :label="$t('updateDate')" :width="180">
+            <template slot-scope="scope">
+                <span>{{moment(parseInt(scope.row.updateDate)).format('YYYY-MM-DD HH:mm:ss')}}</span>
+            </template>
+        </el-table-column>
+        <el-table-column  :label="$t('operation')" :width="200">
+            <template slot-scope="scope">
+                <el-button v-if="false">{{$t('modify')}}</el-button>
+                <el-button @click="onDeleteItem( $event, scope.row.id, scope.row )">{{$t('delete')}}</el-button>
+            </template>
+        </el-table-column>
+
+      </el-table>
+    </el-main>
+		<el-pagination
+		  v-if="listData.length && !loading && listTotal > listPageSize"
+		  background
+		  layout="prev, pager, next"
+		  :total="listTotal"
+		  :page-size="listPageSize"
+		  :current-page="listCurPage"
+		  @current-change="curListChange"
+		  >
+		</el-pagination>
+
+  </el-container>
+</template>
+
+<style>
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+}
+
+.el-header .el-row {
+    margin: 0 auto;
+}
+
+.el-table__row > td {
+    vertical-align: top;
+}
+
+.el-aside {
+    color: #333;
+}
+
+.el-pagination {
+    text-align: right;
+    padding: 10px;
+}
+</style>
+
+<script>
+
+import moment from '@src/chrome/utils/moment.js'
+import config from '@src/chrome/config'
+
+import dataMixin from '@src/mixin/data.js'
+
+const packInfo = require( '@root/package.json' )
+
+export default {
+    mixins: [ dataMixin ]
+    , data() {
+        return {
+            packInfo: packInfo
+        }
+    }
+    , mounted(){
+        let p = this;
+		this.updateFullList( 1, this.$route.query.id );
+    }
+    , methods: {
+        moment
+		, afterUpdateList(){
+			this.loading = false;
+
+            if( ( this.listTotal > this.listPageSize ) ){
+                this.paddingMain = 'padding-bottom: 50px;'
+            }
+		}
+        , onDeleteItem( evt, id, item ){
+            this.loading = 1;
+            this.deleteItem( id, item.md5 ).then( ( )=> {
+                for( let i = 0, j = this.listData.length; i < j; i++ ){
+                    let tmp = this.listData[ i ];
+                    if( tmp.id == id ){
+                        this.listData.splice( i, 1 );
+                        this.loading = 0;
+                        break;
+                    }
+                }
+                console.log( 'this.listData.legnth:', this.listData.length );
+                if( !this.listData.length ){
+                    //this.updateFullList( 1 );
+                    console.log( 'location.href', location.href.split( '?' )[0]+ '?rnd=' + Date.now() );
+                    location.href = location.href.split( '?' )[0]+ '?rnd=' + Date.now();
+                }
+            }).catch( err => {
+                this.loading = 0;
+                console.error( err );
+            });
+        }
+    }
+
+};
+</script>
